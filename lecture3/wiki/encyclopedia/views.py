0823d87b1cb66d2989c7725bb842bd556a10acf9
Joe Goldberg <joe@convoy.com>
2021-05-12 21:53:33 -0700

    requirement 1: entry page

--- lecture3/wiki/encyclopedia/views.py
+++ lecture3/wiki/encyclopedia/views.py
@@ -13,7 +13,11 @@ def index(request):
 def entry(request, title):
     entry_markdown = util.get_entry(title)
     if entry_markdown:
-        return HttpResponse(f"This is the entry for {title}!<br> {markdown2.markdown(entry_markdown)}")
+        # return HttpResponse(markdown2.markdown(entry_markdown))
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdown2.markdown(entry_markdown)
+        })
     else:
         return HttpResponse(f"No entry found for for {title}!")
     # return render(request, "hello/greet.html", {
