b738c54516a66dba8eb90eca8b37ae42ea047933
Joe Goldberg <joe@convoy.com>
2021-05-12 21:44:35 -0700

    checkpoint: entry page

--- lecture3/wiki/encyclopedia/views.py
+++ lecture3/wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +10,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry_markdown = util.get_entry(title)
+    if entry_markdown:
+        return HttpResponse(f"This is the entry for {title}!<br> {markdown2.markdown(entry_markdown)}")
+    else:
+        return HttpResponse(f"No entry found for for {title}!")
+    # return render(request, "hello/greet.html", {
+        # this is the context, all the variables passed to the template
+    #    "name": name.capitalize()
+    # })
