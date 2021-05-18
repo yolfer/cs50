5506fc390cb4998eddeb08d6a5bdef4934b1303e
Joe Goldberg <joe@convoy.com>
2021-05-18 07:31:43 -0700

    add Passenger model, and ability to add Passengers to a Flight

--- lecture4/airline/flights/admin.py
+++ lecture4/airline/flights/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    # on Flight list page, display the following fields in a tabular layout
+    # see http://127.0.0.1:8000/admin/flights/flight/
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    # on Passenger detail page, use a horizontal layout to select/deselect Flights 
+    # see http://127.0.0.1:8000/admin/flights/passenger/<passenger_id>
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
--- lecture4/airline/flights/views.py
+++ lecture4/airline/flights/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponsePermanentRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -11,5 +13,15 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        # TODO error checking
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponsePermanentRedirect(reverse("flight", args=(flight.id,)))
